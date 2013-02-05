1cf3ea6cdc5f42ef3239c26766c7855209e49bde
Devanand <devanand.b@hp.com>
2013-02-05 23:28:20 +0530

    Added copyright and social links

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,4 @@
-$(document).ready(function(){
-	$("#blackbg").click(function(){
-		$("body").addClass("black");
-	});
-	$("#whitebg").click(function(){
-		$("body").removeClass("black");
-	});
+$(document).ready(function(){	
 	$('#navbar').scrollspy();
 	$('[data-spy="scroll"]').each(function () {
 	  var $spy = $(this).scrollspy('refresh')
